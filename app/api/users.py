7060b81d3dae7063a1f8637a47824dcab4e3e304
LaurentBartez <post.thomas@familieknorr.de>
2024-04-28 16:09:14 +0200

    chapter 23: Completed

--- app/api/users.py
+++ app/api/users.py
@@ -2,22 +2,27 @@ from app.api import bp
 from app.models import User
 from app import db
 from app.api.errors import bad_request
-from flask import request, url_for
+from app.api.auth import token_auth
+from flask import request, url_for, abort
 import sqlalchemy as sa
 
 
 @bp.route("/users/<int:id>", methods=["GET"])
+@token_auth.login_required
 def get_user(id):
     return db.get_or_404(User, id).to_dict()
 
 
 @bp.route("/users", methods=["GET"])
+@token_auth.login_required
 def get_users():
     page = request.args.get("page", 1, type=int)
     per_page = min(request.args.get("per_page", 10, type=int), 100)
     return User.to_collection_dict(sa.select(User), page, per_page, "api.get_users")
 
 
+@bp.route("/users/<int:id>/followers", methods=["GET"])
+@token_auth.login_required
 def get_followers(id):
     user = db.get_or_404(User, id)
     page = request.args.get("page", 1, type=int)
@@ -28,6 +33,7 @@ def get_followers(id):
 
 
 @bp.route("/users/<int:id>/following", methods=["GET"])
+@token_auth.login_required
 def get_following(id):
     user = db.get_or_404(User, id)
     page = request.args.get("page", 1, type=int)
@@ -54,7 +60,10 @@ def create_user():
 
 
 @bp.route("/users/<int:id>", methods=["PUT"])
+@token_auth.login_required
 def update_user(id):
+    if token_auth.current_user().id != id:
+        abort(403)
     user = db.get_or_404(User, id)
     data = request.get_json()
     if (
